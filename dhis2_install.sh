a47e8b137779eab1837d7af634e370ed82195aa1
Ivan Gayton <ivangayton@gmail.com>
2017-09-07 14:10:09 +0000

    Starting the postgres install portion

--- dhis2_install.sh
+++ dhis2_install.sh
@@ -3,14 +3,18 @@
 # Setup DHIS on a fresh install of Ubuntu 16.04
 # Assumes you are already a user called dhis with sudo privileges
 
-echo creating configuration folder
-mkdir /home/dhis/config
+if [ ! -d /home/dhis/config ]; then
+    echo creating configuration folder
+    mkdir /home/dhis/config
+fi
 
-echo setting time zone to server location
-sudo dpkg-reconfigure tzdata
-locale -a
-sudo locale-gen nb_NO.UTF-8
+# Set the time zone - this requires user input - to be automated
+#echo setting time zone to server location
+#sudo dpkg-reconfigure tzdata
+#locale -a
+#sudo locale-gen nb_NO.UTF-8
 
+# sudo apt-get install postgresql-9.4
 
 
 
