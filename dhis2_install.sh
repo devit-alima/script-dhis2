60d872c9de6fddf99b95ee8d05e6bd03f09bb92a
Ivan Gayton <ivangayton@gmail.com>
2017-09-07 15:09:40 +0000

    tricky little psql things...

--- dhis2_install.sh
+++ dhis2_install.sh
@@ -3,6 +3,9 @@
 # Setup DHIS on a fresh install of Ubuntu 16.04
 # Assumes you are already a user called dhis with sudo privileges
 
+echo What password do you want for your postgres user?
+read postgres_password
+
 if [ ! -d /home/dhis/config ]; then
     echo creating configuration folder
     mkdir /home/dhis/config
@@ -17,8 +20,18 @@ fi
 # TODO - check if Postgres 9.5 is already installed
 sudo apt-get install -y postgresql-9.5
 
-echo Creating the user dhis and database dhis2
+echo Creating the user dhis
+# This requires a password response from the user
 sudo su - postgres -c "
-psql -U postgres -f /home/dhis/script-dhis2/postgres_setup.sql
+createuser -SDRP dhis
 "
 
+# TODO: change this to read from the config file
+sudo -u postgres psql -c '
+ALTER USER dhis WITH PASSWORD $postgres_password
+'
+
+echo creating the database dhis2
+sudo su - postgres -c "
+createdb -O dhis dhis2
+"
