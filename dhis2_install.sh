0f47715816b0e4e39c7f64773a0e3377093b0f9e
Ivan Gayton <ivangayton@gmail.com>
2017-09-11 13:09:05 +0000

    fiddling with memory settings

--- dhis2_install.sh
+++ dhis2_install.sh
@@ -108,11 +108,6 @@ sudo apt-get update
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
 sudo apt-get install -y oracle-java8-installer
 
-#echo setting up Oracle Java repos and installing Java8
-#sudo add-apt-repository -y ppa:webupd8team/java
-#sudo apt-get -y update
-#sudo apt-get -y install oracle-java8-installer
-
 #sudo apt-get -y install default-jdk
 
 echo installing Tomcat 7
